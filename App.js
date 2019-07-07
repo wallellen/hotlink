04d32f136e094e28b53bcfea769d8e65d177c434
mlike <zhangtuo@wondersoft.cn>
2019-07-08 00:42:20 +0800

    更新成功

--- App.js
+++ App.js
@@ -1,179 +1,165 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   StyleSheet,
-  Dimensions,
-  TouchableOpacity,
   Text,
-  View
+  TouchableOpacity,
+  View,
 } from 'react-native';
-import CodePush from 'react-native-code-push';
+
+import CodePush from "react-native-code-push";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = { restartAllowed: true };
+  }
 
-    this.state = {
-      restartAllowed: true
+  codePushStatusDidChange(syncStatus) {
+    switch(syncStatus) {
+      case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
+        this.setState({ syncMessage: "检查更新中..." });
+        break;
+      case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
+        this.setState({ syncMessage: "下载更新中" });
+        break;
+      case CodePush.SyncStatus.AWAITING_USER_ACTION:
+        this.setState({ syncMessage: "等待用户操作..." });
+        break;
+      case CodePush.SyncStatus.INSTALLING_UPDATE:
+        this.setState({ syncMessage: "安装更新中..." });
+        break;
+      case CodePush.SyncStatus.UP_TO_DATE:
+        this.setState({ syncMessage: "已经是最新版本", progress: false });
+        break;
+      case CodePush.SyncStatus.UPDATE_IGNORED:
+        this.setState({ syncMessage: "用户取消操作", progress: false });
+        break;
+      case CodePush.SyncStatus.UPDATE_INSTALLED:
+        this.setState({ syncMessage: "已经更新至最新版本，重启后生效.", progress: false });
+        break;
+      case CodePush.SyncStatus.UNKNOWN_ERROR:
+        this.setState({ syncMessage: "未知错误.", progress: false });
+        break;
     }
   }
 
-  async sync() {
-    let self = this;
-    try {
-      return await CodePush.sync(
-        {
-          installMode: CodePush.InstallMode.IMMEDIATE,
-          updateDialog: {
-            appendReleaseDescription:true, //是否显示更新description，默认为false
-            descriptionPrefix:"更新内容: ",//更新说明的前缀。 默认是” Description:
-            mandatoryContinueButtonLabel:"立即更新",//强制更新的按钮文字，默认为continue
-            mandatoryUpdateMessage:"",//- 强制更新时，更新通知. Defaults to “An update is available that must be installed.”.
-            optionalIgnoreButtonLabel: '稍后',//非强制更新时，取消按钮文字,默认是ignore
-            optionalInstallButtonLabel: '后台更新',//非强制更新时，确认文字. Defaults to “Install”
-            optionalUpdateMessage: '有新版本了，是否更新？',//非强制更新时，更新通知. Defaults to “An update is available. Would you like to install it?”.
-            title: '更新提示'//要显示的更新通知的标题. Defaults to “Update available”.
-          }
-        },
-        (syncStatus) => {
-          console.log("-syncStatus-" + syncStatus);
-          switch(syncStatus) {
-            case CodePush.SyncStatus.CHECKING_FOR_UPDATE:
-              self.setState({
-                syncMessage: "检查更新..."
-              });
-              break;
-            case CodePush.SyncStatus.DOWNLOADING_PACKAGE:
-              self.setState({
-                syncMessage: "下载更新中..."
-              });
-              break;
-            case CodePush.SyncStatus.AWAITING_USER_ACTION:
-              self.setState({
-                syncMessage: "等待用户操作"
-              });
-              break;
-            case CodePush.SyncStatus.INSTALLING_UPDATE:
-              self.setState({
-                syncMessage: "安装更新中"
-              });
-              break;
-            case CodePush.SyncStatus.UP_TO_DATE:
-              self.setState({
-                syncMessage: "已经是最新版本",
-                progress: false
-              });
-              break;
-            case CodePush.SyncStatus.UPDATE_IGNORED:
-              self.setState({
-                syncMessage: "用户已取消更新",
-                progress: false
-              });
-              break;
-            case CodePush.SyncStatus.UPDATE_INSTALLED:
-              self.setState({
-                syncMessage: "更新完成",
-                progress: false
-              });
-              break;
-            case CodePush.SyncStatus.UNKNOWN_ERROR:
-              self.setState({
-                syncMessage: "未知错误",
-                progress: false
-              });
-              break;
-          }
-        },
-        (progress) => {
-          self.setState({
-            progress: progress
-          });
-        }
-      );
-    } catch (error) {
-      CodePush.log(error);
-    }
+  codePushDownloadDidProgress(progress) {
+    this.setState({ progress });
   }
 
   toggleAllowRestart() {
-    if (this.state.restartAllowed) {
-      CodePush.disallowRestart();
-    } else {
-      CodePush.allowRestart();
-    }
-    this.setState({restartAllowed: !this.state.restartAllowed});
+    this.state.restartAllowed
+      ? CodePush.disallowRestart()
+      : CodePush.allowRestart();
+
+    this.setState({ restartAllowed: !this.state.restartAllowed });
   }
 
-  componentDidMount() {
-    CodePush.notifyApplicationReady();
+  getUpdateMetadata() {
+    CodePush.getUpdateMetadata(CodePush.UpdateState.RUNNING)
+      .then((metadata) => {
+        this.setState({ syncMessage: metadata ? JSON.stringify(metadata) : "Running binary version", progress: false });
+      }, (error) => {
+        this.setState({ syncMessage: "错误: " + error, progress: false });
+      });
   }
-  
+
+  /** Update is downloaded silently, and applied on restart (recommended) */
+  sync() {
+    CodePush.sync(
+      {},
+      this.codePushStatusDidChange.bind(this),
+      this.codePushDownloadDidProgress.bind(this)
+    );
+  }
+
+  /** Update pops a confirmation dialog, and then immediately reboots the app */
+  syncImmediate() {
+    CodePush.sync(
+      { installMode: CodePush.InstallMode.IMMEDIATE, updateDialog: true },
+      this.codePushStatusDidChange.bind(this),
+      this.codePushDownloadDidProgress.bind(this)
+    );
+  }
 
   render() {
-    let syncView, syncButton, progressView;
-
-    if (this.state.syncMessage) {
-      syncView = (
-        <Text style={styles.messages}>{this.state.syncMessage}</Text>
-      );
-    } else {
-      syncButton = (
-        <TouchableOpacity onPress={this.sync.bind(this)}>
-          <Text style={{color: 'green', fontSize: 17}}>开始检测-6</Text>
-        </TouchableOpacity>
-      );
-    }
+    let progressView;
 
     if (this.state.progress) {
       progressView = (
-        <Text style={styles.messages}>已收到 {this.state.progress.receivedBytes}/{this.state.progress.totalBytes}字节数据</Text>
+        <Text style={styles.messages}>已收到{this.state.progress.receivedBytes} of {this.state.progress.totalBytes}字节数据</Text>
       );
     }
 
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          测试更新操作中
+          测试热更代码
         </Text>
-        {syncButton}
-        {syncView}
-        {progressView}
-        {/*<Image style={styles.image} resizeMode={Image.resizeMode.contain} source={require('./images/laptop_phone_howitworks.png')}/>*/}
-        <TouchableOpacity onPress={this.toggleAllowRestart.bind(this)}>
-          <Text style={{color: 'blue', fontSize: 17}}>重启状态 { this.state.restartAllowed ? "允许" : "禁止"}</Text>
+        <TouchableOpacity onPress={this.sync.bind(this)}>
+          <Text style={styles.syncButton}>后台更新</Text>
         </TouchableOpacity>
+        <TouchableOpacity onPress={this.syncImmediate.bind(this)}>
+          <Text style={styles.syncButton}>点击立即更新</Text>
+        </TouchableOpacity>
+        {progressView}
+        <TouchableOpacity onPress={this.toggleAllowRestart.bind(this)}>
+          <Text style={styles.restartToggleButton}>重启 { this.state.restartAllowed ? "开启" : "禁止"}</Text>
+        </TouchableOpacity>
+        {/* <TouchableOpacity onPress={this.getUpdateMetadata.bind(this)}>
+          <Text style={styles.syncButton}>点击检测更新</Text>
+        </TouchableOpacity> */}
+        <Text style={styles.messages}>{this.state.syncMessage || ""}</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  image: {
-    marginTop: 50,
-    width: Dimensions.get('window').width - 100,
-    height: 365 * (Dimensions.get('window').width - 100) / 651,
-  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
     paddingTop: 50
+  },
+  messages: {
+    marginTop: 30,
+    textAlign: "center",
+  },
+  restartToggleButton: {
+    color: "blue",
+    fontSize: 17
+  },
+  syncButton: {
+    color: "green",
+    fontSize: 17
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  messages: {
-    textAlign: 'center',
+    textAlign: "center",
+    margin: 20
   },
 });
+
+/**
+ * Configured with a MANUAL check frequency for easy testing. For production apps, it is recommended to configure a
+ * different check frequency, such as ON_APP_START, for a 'hands-off' approach where CodePush.sync() does not
+ * need to be explicitly called. All options of CodePush.sync() are also available in this decorator.
+ */
+let codePushOptions = { 
+    checkFrequency: CodePush.CheckFrequency.MANUAL,
+    updateDialog: {
+      appendReleaseDescription:true,//是否显示更新description，默认为false
+      descriptionPrefix:"更新内容：",//更新说明的前缀。 默认是” Description:
+      mandatoryContinueButtonLabel:"立即更新",//强制更新的按钮文字，默认为continue  
+      mandatoryUpdateMessage:"",//- 强制更新时，更新通知. Defaults to “An update is available that must be installed.”.
+      optionalIgnoreButtonLabel: '稍后',//非强制更新时，取消按钮文字,默认是ignore
+      optionalInstallButtonLabel: '后台更新',//非强制更新时，确认文字. Defaults to “Install”
+      optionalUpdateMessage: '有新版本了，是否更新？',//非强制更新时，更新通知. Defaults to “An update is available. Would you like to install it?”.
+      title: '更新提示'//要显示的更新通知的标题. Defaults to “Update available”.
+    },
+};
+
+App = CodePush(codePushOptions)(App);
 
 export default App;
